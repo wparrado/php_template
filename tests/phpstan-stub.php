04b1b7fe22c7a9d6af95c29cf1fb54e829613ba6
wparrado <223556219+Copilot@users.noreply.github.com>
2026-05-11 19:49:09 -0500

    chore(phpstan): add stub file for Pest/PHPUnit dynamic test properties

---
+++ tests/phpstan-stub.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * PHPStan test stubs to declare dynamic properties used by Pest closures.
+ * This file is only used by PHPStan via the 'stubFiles' configuration.
+ */
+
+namespace PHPUnit\Framework;
+
+/**
+ * @property mixed $clock
+ * @property mixed $repository
+ * @property mixed $handler
+ * @property mixed $eventPublisher
+ * @property mixed $uow
+ * @property mixed $createHandler
+ * @property mixed $deleteHandler
+ * @property mixed $updateHandler
+ * @property mixed $mapper
+ */
+class TestCase {}
